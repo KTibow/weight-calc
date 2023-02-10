2b83b20b4b442d500764c38936c7feae24db5f1f
Kendell R <KTibow@users.noreply.github.com>
2023-02-10 06:32:19 -0800

    move some stuff around and update the chooser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script>
-  import iconMercury from "$lib/mercury.svg";
-  import iconVenus from "$lib/venus.svg";
-  import iconMoon from "$lib/moon.svg";
-  import iconMars from "$lib/mars.svg";
-  import iconJupiter from "$lib/jupiter.svg";
-  import iconSaturn from "$lib/saturn.svg";
-  import iconUranus from "$lib/uranus.svg";
-  import iconNeptune from "$lib/neptune.svg";
+  import iconApple from "$lib/icons/apple.svg";
+  import iconBasketball from "$lib/icons/basketball.svg";
+  import iconBicycle from "$lib/icons/bicycle.svg";
+  import iconGiraffe from "$lib/icons/giraffe.svg";
+  import iconPerson from "$lib/icons/person.svg";
+
+  import iconMercury from "$lib/planets/mercury.svg";
+  import iconVenus from "$lib/planets/venus.svg";
+  import iconMoon from "$lib/planets/moon.svg";
+  import iconMars from "$lib/planets/mars.svg";
+  import iconJupiter from "$lib/planets/jupiter.svg";
+  import iconSaturn from "$lib/planets/saturn.svg";
+  import iconUranus from "$lib/planets/uranus.svg";
+  import iconNeptune from "$lib/planets/neptune.svg";
   let weight;
   let innerWidth;
   const planets = [
@@ -19,32 +25,68 @@
     { name: "Uranus", factor: 0.92, icon: iconUranus },
     { name: "Neptune", factor: 1.19, icon: iconNeptune },
   ];
+  const exampleObjects = [
+    { icon: iconApple, name: "Apple", kg: 0.15 },
+    { icon: iconBasketball, name: "Basketball", kg: 0.6237 },
+    { icon: iconBicycle, name: "Bicycle", kg: 10 },
+    { icon: iconGiraffe, name: "Giraffe (male)", kg: 1192 },
+    { icon: iconPerson, name: "Person", kg: 70 },
+  ];
+  let kgIn = true;
+  let kgOut = false;
+  $: factor = kgIn == kgOut ? 1 : kgIn ? 2.20462 : 1 / 2.20462;
 </script>
 
 <svelte:window bind:innerWidth />
 <div class="flex flex-col items-center">
   <h1 class="text-4xl lg:text-6xl font-bold text-center my-8">Planet Weight</h1>
-  <div class="flex bg-zinc-800 rounded-xl h-16 overflow-hidden my-8">
-    <input
-      class="bg-inherit px-4 border-r border-zinc-500 [appearance:textfield]"
-      type="number"
-      bind:value={weight}
-    />
-    <span class="self-center px-2">unit of your choice</span>
+  <div class="flex flex-wrap my-8 gap-8 justify-center">
+    <button
+      class="bg-zinc-800 h-16 w-16 rounded-2xl inline-flex items-center justify-center hover:bg-zinc-700 transition-all"
+      on:click={() => (kgIn = !kgIn)}
+    >
+      in<br />{kgIn ? "kg" : "lb"}
+    </button>
+    <button
+      class="bg-zinc-800 h-16 w-16 rounded-2xl inline-flex items-center justify-center hover:bg-zinc-700 transition-all"
+      on:click={() => (kgOut = !kgOut)}
+    >
+      out<br />{kgOut ? "kg" : "lb"}
+    </button>
+    <div class="flex bg-zinc-800 rounded-xl h-16 overflow-hidden">
+      <input
+        class="bg-inherit px-4 border-r border-zinc-500 [appearance:textfield]"
+        type="number"
+        bind:value={weight}
+      />
+      <span class="self-center px-2">{kgIn ? "kg" : "lb"}</span>
+    </div>
+    <div class="flex h-16 border-zinc-500 border-2 rounded-xl overflow-hidden">
+      {#each exampleObjects as { icon, name, kg }}
+        <button
+          class="p-2 hover:bg-zinc-800 transition-all inline-flex flex-col items-center"
+          on:click={() => (weight = (kgIn ? kg : kg * 2.20462).toFixed(2))}
+        >
+          <img src={icon} alt="Icon for apple" class="h-6 w-6" />
+          {name}
+        </button>
+      {/each}
+    </div>
   </div>
   {#if weight}
     <ul class="lg:flex gap-4 lg:absolute lg:bottom-8">
       {#each planets as planet}
+        {@const planetWeight = (weight * planet.factor * factor).toFixed(1)}
         {#if innerWidth < 1024}
           <li class="h-16 flex whitespace-nowrap items-center">
             <img src={planet.icon} alt="Icon for {planet.name}" class="h-16 w-16" />
-            {(weight * planet.factor).toFixed(1)} on {planet.name}
+            {planetWeight}
+            {kgOut ? "kg" : "lb"} on {planet.name}
           </li>
         {:else}
-          {@const planetWeight = (weight * planet.factor).toFixed(1)}
           <li class="flex flex-col self-end items-center">
-            <div class="bg-blue-500 w-20" style="height: {planetWeight}px;" />
-            <span class="text-2xl">{planetWeight}</span>
+            <div class="bg-blue-500 w-20" style="height: {planet.factor * 20}vh;" />
+            <span class="text-2xl">{planetWeight} {kgOut ? "kg" : "lb"}</span>
             <img src={planet.icon} alt="Icon for {planet.name}" class="h-20 w-20" />
             {planet.name}
           </li>
